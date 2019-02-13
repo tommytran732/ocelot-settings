ef31743f58620a0e936675fd4257a7b4eb6d91ac
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-02-12 19:24:10 -0500

    Include dist

---
+++ dist/moduleTest.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    return {
+        moduleTest: () => {
+            return 'module successfully loaded';
+        }
+    };
+})();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kdWxlVGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9tb2R1bGVUZXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxDQUFDLEdBQUcsRUFBRTtJQUNKLE9BQU87UUFDTCxVQUFVLEVBQUUsR0FBRyxFQUFFO1lBQ2YsT0FBTyw0QkFBNEIsQ0FBQztRQUN0QyxDQUFDO0tBQ0YsQ0FBQztBQUNKLENBQUMsQ0FBQyxFQUFFLENBQUMifQ==
