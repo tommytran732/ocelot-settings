d820df8ef78a5e22134cf9dd99776240c7051246
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-02-14 19:14:06 -0500

    Restore OG modTst.js

--- dist/moduleTest.js
+++ dist/moduleTest.js
@@ -1,7 +1,7 @@
 function moduleTest() {
     return {
         moduleTest: () => {
-            return 'Module loaded successfully.';
+            return 'Module successfully loaded.';
         }
     };
 }
