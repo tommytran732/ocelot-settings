32c71a90d21585ae115ff146e79130173af8ade2
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-02-28 17:05:04 -0500

    Callbacks.

--- dist/robotLib.js
+++ dist/robotLib.js
@@ -1,7 +1,9 @@
 function robotLib(config) {
     const sslVisionId = 0, wsClient = new WebSocket('ws://localhost:8000');
     wsClient.onerror = (e) => { config.console.error('Network error.'); };
-    wsClient.onmessage = (m) => { config.console.log(m.data); };
+    wsClient.onmessage = (e) => { config.console.log(e.data); };
+    wsClient.onopen = (e) => { config.console.log('Connected.'); };
+    wsClient.onclose = (e) => { config.console.log('Disconnected.'); };
     function send(payload) {
         wsClient.send(JSON.stringify(payload));
     }
@@ -10,4 +12,4 @@ function robotLib(config) {
             send({ x, y, theta, sslVisionId, cmd: 'MoveTo' });
         }
     };
-} //# sourceURL=robotLib.js`
+}
--- src/robotLib.ts
+++ src/robotLib.ts
@@ -3,10 +3,12 @@ function robotLib(config: any) {
         wsClient: WebSocket = new WebSocket('ws://localhost:8000');
 
   wsClient.onerror = (e) => { config.console.error('Network error.'); };
-  wsClient.onmessage = (m) => { config.console.log(m.data); };
+  wsClient.onmessage = (e) => { config.console.log(e.data); };
+  wsClient.onopen = (e) => { config.console.log('Connected.'); };
+  wsClient.onclose = (e) => { config.console.log('Disconnected.'); };
 
   function send(payload: any) {
-    wsClient.send(JSON.stringify(payload));
+     wsClient.send(JSON.stringify(payload));
   }
 
   return {
