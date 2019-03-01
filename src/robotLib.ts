e62dbfc64d8feeedb175123c1850998bd2f6e1e5
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-02-28 22:17:38 -0500

    WS.

--- src/robotLib.ts
+++ src/robotLib.ts
@@ -1,19 +1,12 @@
 function robotLib(config: any) {
-  const sslVisionId: number = 0,
-        wsClient: WebSocket = new WebSocket('ws://localhost:8000');
-
-  wsClient.onerror = (e) => { config.console.error('Network error.'); };
-  wsClient.onmessage = (e) => { config.console.log(e.data); };
-  wsClient.onopen = (e) => { config.console.log('Connected.'); };
-  wsClient.onclose = (e) => { config.console.log('Disconnected.'); };
 
   function send(payload: any) {
-     wsClient.send(JSON.stringify(payload));
+     config.ws.send(JSON.stringify(payload));
   }
 
   return {
     moveToXY: (x: number, y: number, theta: number) => {
-      send({x, y, theta, sslVisionId, cmd: 'MoveTo'});
+      send({x, y, theta, sslVisionId: 0, cmd: 'MoveTo'});
     }
   };
 }
