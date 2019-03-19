5c67b64588d44da17556ce9bad72d11e31c69f7e
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-03-18 23:21:47 -0400

    On msg update worldState.

--- dist/robotLib.js
+++ dist/robotLib.js
@@ -2,6 +2,10 @@ function robotLib(config) {
     function send(payload) {
         config.ws.send(JSON.stringify(payload));
     }
+    let worldState;
+    config.ws.onmessage = (e) => {
+        worldState = JSON.parse(e.data);
+    };
     return {
         moveToXY: (x, y, theta) => {
             send({ x, y, theta, sslVisionId: 0 });
--- src/robotLib.ts
+++ src/robotLib.ts
@@ -3,6 +3,12 @@ function robotLib(config: any) {
     config.ws.send(JSON.stringify(payload));
   }
 
+  let worldState: any;
+
+  config.ws.onmessage = (e) => {
+    worldState = JSON.parse(e.data);
+  };
+
   return {
     moveToXY: (x: number, y: number, theta: number) => {
       send({x, y, theta, sslVisionId: 0});
