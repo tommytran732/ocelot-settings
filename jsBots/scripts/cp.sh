5c4a52b521b4c7ca092b9c23cf936fe4fdcb39e5
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-06-20 20:40:11 -0400

    Better cp.

--- jsBots/scripts/cp.sh
+++ jsBots/scripts/cp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [[ ! -z $1 ]]; then
   for i in {1..15}; do
-    gsutil -h "Content-Type:text/javascript" cp ../programs/$1 gs://ocelot-student-files/ocelot-$i@cs.umass.edu/
+    gsutil -h "Content-Type:text/javascript" cp $1 gs://ocelot-student-files/ocelot-$i@cs.umass.edu/
   done
 fi
