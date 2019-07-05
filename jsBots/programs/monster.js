2974f7aabb52cbcddbabb60a69e02577e71e0a21
sp1tz <42953063+sp1tz@users.noreply.github.com>
2019-07-05 16:13:04 -0400

    Monsters.

---
+++ jsBots/programs/monster.js
@@ -0,0 +1,15 @@
+let robot = require('robotLibrary');
+robot.setId(1);
+
+while(true) {
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.moveForward();
+  robot.turnRight();
+}
---
+++ jsBots/programs/wileyMonster.js
@@ -0,0 +1,19 @@
+let robot = require('robotLibrary');
+robot.setId(1);
+
+while(true) {
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.moveForward();
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+  robot.moveForward();
+  robot.moveForward();
+  robot.turnRight();
+  robot.moveForward();
+}
